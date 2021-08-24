0ad8ca6776709a821e9bf43482bb2cb110167852
AliFiratKardas <afk.kardas21@gmail.com>
2021-08-24 14:09:58 -0700

    MVCProject

---
+++ InsaatMVC/InsaatMVC/Controllers/CementController.cs
@@ -0,0 +1,26 @@
+﻿using InsaatMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsaatMVC.Controllers
+{
+    public class CementController : Controller
+    {
+        InsaatMalzemeleriEntities db = new InsaatMalzemeleriEntities();
+        // GET: Cement
+        public ActionResult Index()
+        {
+            
+            List<Cement> cements= db.Cements.ToList();
+            return View(cements);
+        }
+        public ActionResult Details(int id)
+        {
+            var cement = db.Cements.Where(x => x.ID == id).FirstOrDefault();
+            return View(cement);
+        }
+    }
+}
---
+++ InsaatMVC/InsaatMVC/Controllers/SteelController.cs
@@ -0,0 +1,27 @@
+﻿using InsaatMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsaatMVC.Controllers
+{
+    
+    public class SteelController : Controller
+    {
+        InsaatMalzemeleriEntities db = new InsaatMalzemeleriEntities();
+        // GET: Steel
+        public ActionResult Index()
+        {
+            List<Steel> steels = db.Steels.ToList();
+
+            return View(steels);
+        }
+        public ActionResult Details(int id)
+        {
+            var steel = db.Steels.Where(x => x.ID == id).FirstOrDefault();
+            return View(steel);
+        }
+    }
+}
